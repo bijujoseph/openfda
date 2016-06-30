54e469f22bcca8def301c0187b3cc3e32dabf875
Biju Joseph <biju.joseph@semanticbits.com>
2016-06-30 18:28:56 -0400

    FDA-13 - quick start

--- quickstart.sh
+++ quickstart.sh
@@ -3,7 +3,7 @@
 ## Create an AWS EC2 instance from Amazon linux AMI (as it has Docker)
 ## mkdir /work
 ## cd /work
-## yum install git
+## yum install git gcc
 ## git clone https://github.com/bijujoseph/openfda.git
 ## cd openfda
 ## git checkout -b feature/FDA-13 --track origin/feature/FDA-13
@@ -21,7 +21,7 @@ cd /tmp && \
   cp -R /tmp/elasticsearch-1.7.1/* /elasticsearch/
 sudo mkdir /data0 && sudo chmod 777 /data0/
 cp /work/openfda/deploy/elasticsearch-dev/*.yml /elasticsearch/config/
-
+cd /work/openfda/
 ./bootstrap.sh
 
 
