75f4a91e252cecfac4c697bfde612235322275c1
Biju Joseph <biju.joseph@semanticbits.com>
2016-07-01 10:58:13 -0400

    FDA-13 - quick start

--- quickstart.sh
+++ quickstart.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 
+##
 ## Create an AWS EC2 instance from Amazon linux AMI and run the following commands in the comments
-
-## sudo mkdir /work && sudo chmod 777 /work
-## cd /work
-## sudo yum install -y git
-## git clone https://github.com/bijujoseph/openfda.git
-## cd openfda
-## git checkout -b feature/FDA-13 --track origin/feature/FDA-13
-## chmod 777 *.sh
-## ./quickstart.sh
+## You can run this on server using the following command:
+## curl --silent --location https://raw.githubusercontent.com/bijujoseph/openfda/feature/FDA-13/quickstart.sh | bash -
 ##
 
 
+# --------------------------------
+# Step 0 - Setup environment
+# --------------------------------
+
+sudo mkdir -p /work/outputs/ && sudo chmod -R 777 /work
+sudo mkdir /data0 && sudo chmod 777 /data0/
+
+cd /work
+cat<<EOF > /work/apiserver.sh
+#!/bin/bash
+cd /work/openfda/api/faers
+npm run start
+EOF
+chmod 777 apiserver.sh
+
+#---------------------------------
 # Step 1 - Install Node.js & NPM
+# --------------------------------
 
 sudo curl --silent --location https://rpm.nodesource.com/setup | bash -
-sudo yum install -y nodejs npm gcc
+sudo yum install -y git nodejs npm gcc
+sudo mkdir -p /work && sudo chmod 777 /work && mkdir -p /work/output/
+cd /work
+git clone https://github.com/bijujoseph/openfda.git
+cd /work/openfda
+git checkout -b feature/FDA-13 --track origin/feature/FDA-13
+chmod 777 *.sh
 
-
+#---------------------------------
 # Step 2 - Install ElasticSearch
+# --------------------------------
 
 cd /tmp && \
   wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.1.tar.gz && \
@@ -31,18 +49,33 @@ cd /tmp && \
 
 chmod 777 /elasticsearch/bin/* && \
 
-sudo mkdir /data0 && sudo chmod 777 /data0/
 cp /work/openfda/deploy/docker/elasticsearch-dev/*.yml /elasticsearch/config/
 cd /work/openfda/
 
 
+#------------------------------------------------
 #Step 3 - Compile backoffice python & API website
+#------------------------------------------------
+
 ./bootstrap.sh
 
-# Step 4 - Start in background API Server
+# Step 4 - Start in background EasticSearch Server
+echo 'Starting ElasticSearch server, you can check the output in /work/outputs/apiserver.out'
+cd /elasticsearch/bin
+nohup ./elasticsearch > /work/outputs/elasticsearch.out &
 
-# Step 5 - Start in background EasticSearch Server
+#---------------------------------
+# Step 5 - Start API server
+#---------------------------------
+sleep 20
+echo 'Starting API server, you can check the output in /work/outputs/apiserver.out'
 
+cd /work
+nohup ./apiserver.sh > /work/outputs/apiserver.out &
+
+
+# --------------------------------
 # Step 6 - Run all pipelines
-
-
+# --------------------------------
+ehco 'To run pipelines run the following command : '
+echo './scripts/all-pipelines.sh'
