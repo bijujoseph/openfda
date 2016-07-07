3acb91a8b4b8a65d442eb018868e048355d5b249
Biju Joseph <biju.joseph@semanticbits.com>
2016-07-07 12:04:54 -0400

    Updated S3 URL based on https://github.com/FDA/openfda/issues/72

--- quickstart.sh
+++ quickstart.sh
@@ -10,7 +10,7 @@
 # --------------------------------
 # Step 0 - Setup environment
 # --------------------------------
-
+echo 'Step 0 - Creating work folders'
 sudo mkdir -p /work/outputs/ && sudo chmod -R 777 /work
 sudo mkdir /data0 && sudo chmod 777 /data0/
 
@@ -25,11 +25,16 @@ chmod 777 apiserver.sh
 #---------------------------------
 # Step 1 - Install Node.js & NPM
 # --------------------------------
+echo 'Step 1 - Installing the source code and other required softwares'
 
-sudo curl --silent --location https://rpm.nodesource.com/setup | bash -
-sudo yum install -y git gcc
-sudo yum install -y nodejs npm --enablerepo=epel
-sudo mkdir -p /work && sudo chmod 777 /work && mkdir -p /work/output/
+sudo apt-get update
+sudo apt-get install -y git gcc nodejs npm python-pip libxml2-dev libxslt1-dev python2.7-dev
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get install -y oracle-java8-installer
+sudo ln -s /usr/bin/nodejs /usr/sbin/node
+sudo mkdir -p /work && sudo chmod 777 /work && mkdir -p /work/outputs/
 cd /work
 git clone https://github.com/bijujoseph/openfda.git
 cd /work/openfda
@@ -40,6 +45,7 @@ chmod 777 *.sh
 # Step 2 - Install ElasticSearch
 # --------------------------------
 
+echo 'Step 2 - Installing Elastic Search'
 cd /tmp && \
   wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.1.tar.gz && \
   tar xvzf elasticsearch-1.7.1.tar.gz && \
@@ -57,10 +63,12 @@ cd /work/openfda/
 #------------------------------------------------
 #Step 3 - Compile backoffice python & API website
 #------------------------------------------------
-
+echo 'Step 3 - running bootstrap.sh'
 ./bootstrap.sh
 
+#------------------------------------------------
 # Step 4 - Start in background EasticSearch Server
+#------------------------------------------------
 echo 'Starting ElasticSearch server, you can check the output in /work/outputs/apiserver.out'
 cd /elasticsearch/bin
 nohup ./elasticsearch > /work/outputs/elasticsearch.out &
@@ -77,6 +85,12 @@ nohup ./apiserver.sh > /work/outputs/apiserver.out &
 
 # --------------------------------
 # Step 6 - Run all pipelines
+# --------------------------------
+echo 'Configure profile manually using:'
+echo 'aws configure profile=openfda'
+
+# --------------------------------
+# Step 7 - Run all pipelines
 # --------------------------------
 echo 'To run pipelines run the following command : '
 echo './scripts/all-pipelines.sh'
